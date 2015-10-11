d893fca04246543ad73d72d049b470face9a89bb
Ziv Karliner <zivkr2@gmail.com>
2015-10-11 17:43:08 +0300

    Lock screen Added
    Added the lock screen design and resources to the project

---
+++ app/src/main/java/com/red/alert/activities/settings/SdlLockscreen.java
@@ -0,0 +1,18 @@
+package com.red.alert.activities.settings;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.red.alert.R;
+
+/**
+ * Created by ziv on 11/10/2015.
+ */
+public class SdlLockscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sdl_lockscreen);
+    }
+}
